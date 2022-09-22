b2142b961ea0b5a3f39adb7cc203cd8ff74853db
Prefect <prefect@pool-5-79-186-204.is74.ru>
2022-09-22 17:56:32 +0500

    v1.0

---
+++ T1/src/main.cpp
@@ -0,0 +1,28 @@
+#include "Headings.h"
+
+using namespace std;
+
+int main() {
+    const unsigned int width = 17;
+
+    int count = 0;
+    int num;
+    
+    cin.unsetf(ios::dec);
+    cin.setf(ios::hex);
+    
+    while (count != 2)
+    {
+        cout.width(width);
+        cout << "$$$$$$$$" << endl;
+
+        cin >> num;
+
+        if (num == 16) count++;
+
+        string result = (num == 16) ? "true" : "false";
+        
+        cout.width(width);
+        cout << result << endl;
+    }
+}
